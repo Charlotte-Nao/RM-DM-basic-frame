6b1c0491683c78419c23a91f2daf29756eb1130a
Charlotte-Nao <2590732913@qq.com>
2026-07-21 00:40:25 +0800

    标定完毕，硬编码效果良好

--- task/test_task/test_task.c
+++ task/test_task/test_task.c
@@ -52,6 +52,39 @@ static void put_chess(struct vacuum_device *pump, struct vacuum_device *valve)
     pump->disable(pump);
 }
 
+static void move_a_to_b(struct yb_sd15m_device *servo_1,
+                        struct yb_sd15m_device *servo_2,
+                        struct yb_sd15m_device *servo_3,
+                        struct yb_sd15m_device *servo_4,
+                        const float a_pose[4],
+                        const float b_pose[4])
+{
+    float a_above_pose[4];
+    float b_above_pose[4];
+    float servo_angle[4] = {0};
+
+    if (a_pose == NULL || b_pose == NULL) {
+        return;
+    }
+
+    memcpy(a_above_pose, a_pose, sizeof(a_above_pose));
+    memcpy(b_above_pose, b_pose, sizeof(b_above_pose));
+    if (a_above_pose[2] < 120 ){   a_above_pose[2] += 30.0f;}
+    if (b_above_pose[2] < 120 ){   b_above_pose[2] += 30.0f;}
+
+    Four_degree_of_freedom_calculation(&arm, a_above_pose, servo_angle);
+    send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
+    osDelay(1000U);
+
+    Four_degree_of_freedom_calculation(&arm, b_above_pose, servo_angle);
+    send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
+    osDelay(1000U);
+
+    Four_degree_of_freedom_calculation(&arm, b_pose, servo_angle);
+    send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
+    osDelay(2000U);
+}
+
 void test_task(void)
 {
     struct yb_sd15m_device *servo_1;
@@ -80,13 +113,13 @@ void test_task(void)
     float zero_pose[4] =    {160.0f,    0.0f,    180.0f, 75.0f};
     float one_pose[4] =     {250.0f,    15.0f,    75.0f, 75.0f};
     float two_pose[4] =     {250.0f,    - 25.0f, 75.0f, 75.0f};
-    float three_pose[4] =   {250.0f,    - 55.0f, 75.0f, 75.0f};
+    float three_pose[4] =   {245.0f,    - 55.0f, 75.0f, 75.0f};
     float four_pose[4] =    {225.0f,    15.0f,   65.0f, 75.0f};
     float five_pose[4] =    {225.0f,    -25.0f,    65.0f, 75.0f};
-    float six_pose[4] =     {225.0f,    - 55.0f, 65.0f, 75.0f};
-    float seven_pose[4] =   {200.0f,    15.0f,   62.0f, 75.0f};
-    float eight_pose[4] =   {200.0f,    -25.0f,    62.0f, 75.0f};
-    float nine_pose[4] =    {200.0f,    - 57.0f, 62.0f, 75.0f};
+    float six_pose[4] =     {225.0f,    - 55.0f, 62.0f, 75.0f};
+    float seven_pose[4] =   {200.0f,    20.0f,   62.0f, 75.0f};
+    float eight_pose[4] =   {200.0f,    -20.0f,    62.0f, 75.0f};
+    float nine_pose[4] =    {200.0f,    - 55.0f, 62.0f, 75.0f};
 
     float *board_pose_list[10] = {
         zero_pose,
@@ -103,16 +136,16 @@ void test_task(void)
 
     float black_one[4]   =    {250.0f,   60.0f,  80.0f,  75.0f};
     float black_two[4]   =    {225.0f,   65.0f,  65.0f,  75.0f};
-    float black_three[4] =    {200.0f,   70.0f,  65.0f,  75.0f};
-    float black_four[4]  =   {175.0f,    75.0f,  65.0f,  75.0f};
-    float black_five[4]  =   {140.0f,    85.0f,  65.0f,  75.0f};
+    float black_three[4] =    {200.0f,   70.0f,  60.0f,  75.0f};
+    float black_four[4]  =   {175.0f,    75.0f,  60.0f,  75.0f};
+    float black_five[4]  =   {140.0f,    85.0f,  60.0f,  75.0f};
     float white_one[4]   =  {240.0f,    - 105.0f,75.0f,  75.0f};
     float white_two[4]   =  {220.0f,    - 105.0f,    67.0f,  75.0f};
     float white_three[4] =  {190.0f ,   - 110.0f,    65.0f,  75.0f};
     float white_four[4]  =  {165.0f ,   - 115.0f,    60.0f,  75.0f};
     float white_five[4]  =  {135.0f ,   - 120.0f,   60.0f,  75.0f};
 
-    float *chess_pos_list[10] = {
+    float *chess_pose_list[10] = {
         black_one,
         black_two,
         black_three,
@@ -146,36 +179,25 @@ void test_task(void)
         // send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
         // osDelay(2000U);
 
-        //
-        // Four_degree_of_freedom_calculation(&arm,chess_pos_list[temp_j],servo_angle);
-        // temp_j++;
-        // temp_j = temp_j % 10;
-        // send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
-        // pick_chess(pump,valve);
-        // osDelay(3000U);
-        //
-        //
-        // osDelay(2000U);
-        //
-        // Four_degree_of_freedom_calculation(&arm, board_pose_list[temp_i], servo_angle);
-        // temp_i ++ ;
-        // temp_i = temp_i % 10;
-        // if (temp_i == 0)
-        // {
-        //     temp_i ++;
-        // }
-        // send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
-        // osDelay(3000U);
-        //
-        // put_chess(pump,valve);
-        // osDelay(2000U);
-        //
-        // Four_degree_of_freedom_calculation(&arm, board_pose_list[0], servo_angle);
-        // send_all_servo(servo_1, servo_2, servo_3, servo_4, servo_angle, 1000U);
-        // osDelay(3000U);
+        move_a_to_b(servo_1, servo_2, servo_3, servo_4,board_pose_list[0], chess_pose_list[temp_j]);
+        pick_chess(pump,valve);
+        osDelay(2000U);
 
+        move_a_to_b(servo_1,servo_2,servo_3,servo_4,chess_pose_list[temp_j],board_pose_list[temp_i]);
+        put_chess(pump,valve);
+        osDelay(2000U);
 
+        move_a_to_b(servo_1,servo_2,servo_3,servo_4,board_pose_list[temp_i],board_pose_list[0]);
 
+        temp_j++;
+        temp_j = temp_j % 10;
+
+        temp_i ++ ;
+        temp_i = temp_i % 10;
+        if (temp_i == 0)
+        {
+            temp_i ++;
+        }
 
     }
 }
