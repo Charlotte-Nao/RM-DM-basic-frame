c74db64e75742bfb6425d8b84cd07dc2db0b7b1e
Charlotte-Nao <2590732913@qq.com>
2026-08-27 21:49:14 +0800

    8009P测试完毕

--- task/test_task/test_task.c
+++ task/test_task/test_task.c
@@ -17,10 +17,21 @@
 
 void test_task(void)
 {
-
+    // struct motor_device *DM_8009P;
+    // DM_8009P = motor_get_device("DM8009P_1");
+    // if (DM_8009P == NULL) { LED_RED_SET(); return; }
+    // DM_8009P->send_enable_cmd(DM_8009P);
+    //
+    // osDelay(100U);
+    // DM_8009P->set_target(DM_8009P, 1, 1.5f);
+    // uint32_t next_wake_tick;
+    // next_wake_tick = osKernelGetTickCount();
     for (;;) {
-            osDelay(1U);
-
-        }
-
+        // next_wake_tick += 2000U;
+        // (void)osDelayUntil(next_wake_tick);
+        // DM_8009P->set_target(DM_8009P, 1, -1.5f);
+        // next_wake_tick += 2000U;
+        // (void)osDelayUntil(next_wake_tick);
+        // DM_8009P->set_target(DM_8009P, 1, 1.5f);
+    }
 }
